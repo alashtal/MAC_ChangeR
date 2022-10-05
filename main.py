3916d50ede60b5816cd0e9318f341a66bda4754c
ashtal <alashtal.mohammed@gmail.com>
2022-10-05 12:31:59 -0400

    Added functions

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ import subprocess
 import optparse
 
 # -h or --help can help you see how to use the program
-parse = optparse.OptionParser()
-parse.add_option("-i", "--interface", dest="interface", help="The interface you wish to change its MAC address")
-parse.add_option("-m", "--mac", dest="new_MAC", help="The new MAC address you wish to change to")
-
-(options, arguments) = parse.parse_args()
-
-interface = options.interface
-new_mac = options.new_MAC
 
 
-print("[+]Changing MAC address of " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print("[+]Changing MAC address of " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
 
-# Oldie but goldie
+def get_arguments():
+    parse = optparse.OptionParser()
+    parse.add_option("-i", "--interface", dest="interface", help="The interface you wish to change its MAC address")
+    parse.add_option("-m", "--mac", dest="new_MAC", help="The new MAC address you wish to change to")
+
+    (options, arguments) = parse.parse_args()
+    if not options.interface:
+        parse.error("[-] Please enter an interface '-i' or check '-h' for further help ")
+    elif not options.new_MAC:
+        parse.error("[-] Please enter an interface '-i' or check '-h' for further help ")
+    return options
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_MAC)
+
+print(subprocess.call("ifconfig"))
+
+
+# Old code
 # interface = input("heyy")
 # subprocess.call("ifconfig " + interface + " down", shell=True)
 # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
 # subprocess.call("ifconfig " + interface + " up", shell=True)
-
-print(subprocess.call("ifconfig"))
